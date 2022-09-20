45aff633bc683946abaf41f7ce7f898a573db5b7
Mariya SOSNOVA <msosnova@e1r7p2.clusters.42paris.fr>
2022-09-20 16:14:16 +0200

    20.09

--- vector.hpp
+++ vector.hpp
@@ -16,31 +16,55 @@
 
 #include <iostream>
 #include <string>
+#include <memory>
 
 namespace ft 
 {
 	template <class T, class Allocator = std::allocator<T> >
 	class vector {
-		protected:
+		private: // attributes
+			size_type		_capacity; 
+			pointer			_begin;
+			pointer			_end;
+			allocator_type	_allocator;
 
-		public:
-		// types:
-			typedef typename Allocator::reference			reference;
-			typedef typename Allocator::const_reference 	const_reference;
-			typedef implementation defined iterator; // See 23.1
-			typedef implementation defined const_iterator; // See 23.1
-			typedef implementation defined size_type; // See 23.1
-			typedef implementation defined difference_type;// See 23.1
-			//typedef T 										value_type;
-			//typedef Allocator 								allocator_type;
-			typedef typename Allocator::pointer 			pointer;
-			typedef typename Allocator::const_pointer 		const_pointer
-			typedef std::reverse_iterator<iterator> 		reverse_iterator;
-			typedef std::reverse_iterator<const_iterator> 	const_reverse_iterator;
+		public: // types:
+			typedef T 													value_type;  	// The template parameter (T)
+			typedef Allocator 											allocator_type; // The template parameter (Allocator). Defaults to: allocator<value_type>
+			typedef typename Allocator::reference						reference; 		// a reference to an element stored in a vector. For the default allocator: value_type&
+			typedef typename Allocator::const_reference 				const_reference;// give a reference to a const element stored. For the default allocator: const value_type&
+			typedef typename Allocator::pointer 						pointer;		// a pointer to an element stored. Can be used to modify the value of an element. For the default allocator: value_type*
+			typedef typename Allocator::const_pointer 					const_pointer;  //a const pointer to an element stored. Can't be used to modify the value of an element.
+				
+			typedef std::size_t											size_type; // See 23.1
+			typedef std::ptrdiff_t										difference_type;// See 23.1
+
+			typedef std::random_access_iterator<value_type> 			iterator; // See 23.1
+			typedef std::random_access_iterator<const value_type>	 	const_iterator; // See 23.1
+			typedef std::reverse_iterator<iterator> 					reverse_iterator;  // can read or modify any element in a reversed vector.
+			typedef std::reverse_iterator<const_iterator> 				const_reverse_iterator; //can read any element in a reversed the vector. Can't be used to modify.
 		
 		//constructor
-			explicit vector(const Allocator& = Allocator()); //default: Constructs an empty container, with no elements.
-			explicit vector(size_type n, const T& value = T(), const Allocator& = Allocator()); //fill: Constructs a container with n elements. Each element is a copy of value.
+			explicit vector(const Allocator& allocator = Allocator())	//default: Constructs an empty container, with no elements.
+			{
+				_capacity = u_nullptr;
+				_begin = u_nullptr;
+				_end = u_nullptr;
+				_allocator = allocator;
+
+			}															 
+			
+			explicit vector(size_type n, const T& value = T(), const Allocator& allocator = Allocator())
+			{
+				_allocator = allocator;									//fill: Constructs a container with n elements. Each element is a copy of value.
+				_start = _allocatot.allocate(n);
+				_end = _start;
+				while (n--)
+				{
+					_allocate.construct(_end, value);
+					_end++;
+				}
+			}
 
 			template <class InputIterator>
 			vector(InputIterator first, InputIterator last, const Allocator& = Allocator()); //range: Constructs a container with as many elements as the range [first,last), 
@@ -50,12 +74,7 @@ namespace ft
 
 			~vector();
 
-			vector<T,Allocator>& operator=(const vector<T,Allocator>& x);
-
-			template <class InputIterator>
-				void assign(InputIterator first, InputIterator last);
-				void assign(size_type n, const T& u);
-
+			vector<T,Allocator>& operator=(const vecthttps://github.com/rchallie/ft_containers/blob/master/containers/vector.hpp
 			allocator_type get_allocator() const;
 		// iterators:
 			iterator begin();
@@ -123,5 +142,9 @@ namespace ft
 
 
 #endif
+
+https://github.com/Conanyedo/Ft_Containers/blob/master/vector.hpp
+
+https://github.com/rchallie/ft_containers/blob/master/containers/vector.hpp
 
 https://github.com/Conanyedo/Ft_Containers/blob/master/vector.hpp
