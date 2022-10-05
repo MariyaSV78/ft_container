c3bffec0e86ebe9096b5952bad59169b65c5ac90
Mariya SOSNOVA <msosnova@e2r3p2.clusters.42paris.fr>
2022-10-05 19:10:44 +0200

    05_09_22

---
+++ utils/pair.hpp
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pair.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msosnova <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/05 16:45:44 by msosnova          #+#    #+#             */
+/*   Updated: 2022/10/05 16:45:58 by msosnova         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PAIR_HPP
+# define PAIR_HPP
+
+namespace ft
+{
+	template<typename T1, typename T2>
+		struct  pair
+		{
+			typedef T1	first_type;
+			typedef T2	second_type;
+
+			T1		first;
+			T2		second;
+
+			pair(): first(T1()), second(T2()) {}
+
+			pair(const T1& x, const T2& y);
+
+			template<class U, class V> 
+				pair(const pair<U, V> &p);
+
+			pair&	operator=(const pair<T1, T2>& p)
+				{
+					first = p.first;
+					second = p.second;
+					return *this;
+				}
+
+			template <class T1, class T2>
+				bool operator==(const pair<T1, T2>& x, const pair<T1, T2>& y)
+				{return (x.first == y.first && x.second == y.second)}
+
+			template <class T1, class T2>
+				bool operator<(const pair<T1, T2>& x, const pair<T1, T2>& y)
+				{return (x.first < y.first || (!(y.first < x.first) && x.second < y.second))}
+			
+			template <class T1, class T2>
+				bool operator>(const pair<T1, T2>& x, const pair<T1, T2>& y)
+				{return (x.first > y.first || (!(y.first > x.first) && x.second > y.second))}
+
+			template <class T1, class T2>
+				bool operator!=(const pair<T1, T2>& x, const pair<T1, T2>& y)
+				{return !(x == y)}
+
+			template <class T1, class T2>
+				bool operator>=(const pair<T1, T2>& x, const pair<T1, T2>& y)
+				{return !(x < y)}
+
+			template <class T1, class T2>
+				bool operator<=(const pair<T1, T2>& x, const pair<T1, T2>& y)
+				{return !(y < x)}
+
+				void swap(pair<T1, T2>& x, pair<T1, T2>& y)
+				{ 
+					pair<T1,T2>		tmp;
+
+					tmp = x;
+					x = y;
+					y = tmp;
+				}
+
+			template <class T1, class T2>
+				pair<T1, T2> make_pair(const T1& x, const T2& y)
+				{return (pair<T1, T2>(x, y))}
+		};
+		
+}
+
+#endif
+
